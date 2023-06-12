f05afa2e506bc611693f8261adf767aacd635ab6
kelechi-aims <kelechi@aims.edu.gh>
2023-06-12 12:18:53 +0100

    task-8 correction

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 def magic_calculation(a, b):
-#    from magic_calculation_10 import add, sub
-    add = __import__('magic_calculation_02', globals(), locals(), ['add'], 0).add
-    sub = __import__('magic_calculation_02', globals(), locals(), ['sub'], 0).sub
-
+    from magic_calculation_10 import add, sub
     if a < b:
         c = add(a, b)
         for i in range(4, 6):
