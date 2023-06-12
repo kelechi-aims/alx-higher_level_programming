57ab5f4836c09a3235351fed8f8392c3d44eb2b2
kelechi-aims <kelechi@aims.edu.gh>
2023-06-12 05:45:31 +0100

    task-6 correction

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for i in matrix:
-        for j in i:
-            print("{:d}".format(j), end=" ")
+    for row in range(len(matrix)):
+        for num in range(len(matrix[row])):
+            if num != 0:
+                print(" ", end='')
+            print("{:d}".format(matrix[row][num]), end='')
         print()
