0d21abf78927bb054a5f6f75a9720a7841c9faa9
kelechi-aims <kelechi@aims.edu.gh>
2023-06-12 06:11:28 +0100

    task-8

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
+    """
+    adds 2 tuples, we append (0, 0) to ensure the each
+    tuple has the length of at least 2
+    """
     a = tuple_a + (0, 0)
     b = tuple_b + (0, 0)
     return (a[0] + b[0], a[1] + b[1])
