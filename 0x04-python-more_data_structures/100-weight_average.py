8387343b5f657949a534a56588776f5802fc1790
kelechi-aims <kelechi@aims.edu.gh>
2023-06-15 04:58:02 +0100

    task-8

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if not my_list:
+        return 0
+    sum_product = 0
+    total_weight = 0
+    for score, weight in my_list:
+        sum_product += score * weight
+        total_weight += weight
+        average = sum_product / total_weight
+    return average
