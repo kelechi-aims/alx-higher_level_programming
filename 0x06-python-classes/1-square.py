c72d8ca3a7976a4fadb9c3c30c841110435b0da3
kelechi-aims <kelechi@aims.edu.gh>
2023-06-28 02:48:12 +0100

    task-1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,2 +1,23 @@
 #!/usr/bin/python3
+""" A class Square that defines a square by:
+    Private instance attribute: size
+    stantiation with size (no type/value verification)
+"""
+
+
 class Square:
+    """A class that defines a square.
+
+    Attributes:
+        __size (int): The size of the square (private).
+
+    """
+
+    def __init__(self, size):
+        """Initialize a Square instance.
+
+        Args:
+            size (int): The size of the square.
+
+        """
+        self.__size = size
