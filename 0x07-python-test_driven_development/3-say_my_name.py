7e19e38a33725c1825058c5347c97480fe72aaa6
kelechi-aims <kelechi@aims.edu.gh>
2023-07-03 16:10:13 +0100

    upload task-2

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+'''
+   The function in this module prints My name is <first name> <last name>.
+   In the above format
+
+'''
+
+
+def say_my_name(first_name, last_name=""):
+    '''
+    Prints the name based on the first name and last name.
+
+    Args:
+        first_name (str): The first name.
+        last_name (str, optional): The last name. Default to an empty string.
+
+    Raises:
+        TypeError: If either first_name or last-name is not a string.
+
+    Returns:
+        None
+    '''
+
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+    if last_name:
+        print("My name is {} {}". format(first_name, last_name))
+    else:
+        print("My name is {}".format(first_name))
