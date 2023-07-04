7a20eef54e6b6e03ff33641679f3f52a1079431e
kelechi-aims <kelechi@aims.edu.gh>
2023-07-04 08:16:16 +0100

    fixed cpde fail

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -95,10 +95,12 @@ if __name__ == "__main__":
         print("Usage: nqueens N")
         sys.exit(1)
 
-    if sys.argv[1].isdigit() is False:
+    try:
+        N = int(sys.argv[1])
+    except ValueError:
         print("N must be an integer")
         sys.exit(1)
-    if int(sys.argv[1]) < 4:
+    if N < 4:
         print("N must be at least 4")
         sys.exit(1)
 
