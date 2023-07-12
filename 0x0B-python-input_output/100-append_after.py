57d9592edeaba9a2a5746681634c247675cf76a9
kelechi-aims <kelechi@aims.edu.gh>
2023-07-12 04:34:43 +0100

    task-13

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+'''Has function that inserts a line of text to a file, after each line
+   containing a specific string
+'''
+
+
+def append_after(filename="", search_string="", new_string=""):
+    '''
+    Inserts a line of text after each line conataining a specific
+    string in a file
+
+    Args:
+        filename (str): The name of the file.
+        search_string (str): The string to search for in each.
+        new_string (str): The line of text to insert after each matching
+                          line.
+    '''
+    lines = []
+    with open(filename, mode='r', encoding='utf-8') as file:
+        for line in file:
+            lines.append(line.rstrip())
+            if search_string in line:
+                lines.append(new_string)
+
+    with open(filename, mode='w', encoding='utf-8') as file:
+        file.write("".join(lines))
