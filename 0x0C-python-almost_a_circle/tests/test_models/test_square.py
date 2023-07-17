e419c03f0407da8f2bc105d5e7119ca23e2186e0
kelechi-aims <kelechi@aims.edu.gh>
2023-07-18 00:55:45 +0100

    task-10

--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -26,11 +26,12 @@ class TestSquare(unittest.TestCase):
         '''
         Test constructor with x and y arguments.
         '''
-        s = Square(5, 2, 3)
-        self.assertEqual(s.width, 5)
-        self.assertEqual(s.height, 5)
+        s = Square(10, 2, 3, 42)
+        self.assertEqual(s.width, 10)
+        self.assertEqual(s.height, 10)
         self.assertEqual(s.x, 2)
         self.assertEqual(s.y, 3)
+        self.assertEqual(s.id, 42)
 
     def test_area(self):
         '''
@@ -51,6 +52,9 @@ class TestSquare(unittest.TestCase):
 
         s = Square(3, 1, 3)
         self.assertEqual(str(s), "[Square] (2) 1/3 - 3")
+
+        s = Square(5, 1, 2, 10)
+        self.assertEqual(str(s), "[Square] (10) 1/2 - 5")
 
     def test_display(self):
         '''
@@ -74,20 +78,49 @@ class TestSquare(unittest.TestCase):
         '''
         s = Square(5)
 
-        s.update(10)
-        self.assertEqual(str(s), "[Square] (10) 0/0 - 5")
+        s.update(20)
+        self.assertEqual(s.id, 20)
 
-        s.update(10, 3)
-        self.assertEqual(str(s), "[Square] (10) 0/0 - 3")
+        s.update(25, 8)
+        self.assertEqual(s.id, 25)
+        self.assertEqual(s.width, 8)
+        self.assertEqual(s.height, 8)
 
-        s.update(10, 3, 2)
-        self.assertEqual(str(s), "[Square] (10) 2/0 - 3")
+        s.update(30, 4, 3)
+        self.assertEqual(s.id, 30)
+        self.assertEqual(s.width, 4)
+        self.assertEqual(s.height, 4)
+        self.assertEqual(s.x, 3)
 
-        s.update(10, 3, 2, 4)
-        self.assertEqual(str(s), "[Square] (10) 2/4 - 3")
+        s.update(35, 2, 2, 2)
+        self.assertEqual(s.id, 35)
+        self.assertEqual(s.width, 2)
+        self.assertEqual(s.height, 2)
+        self.assertEqual(s.x, 2)
+        self.assertEqual(s.y, 2)
 
-        s.update(10, 3, 4, 5)
-        self.assertEqual(str(s), "[Square] (10) 4/5 - 3")
+    def test_update_kwargs(self):
+        """Test the update method of the Square class using **kwargs."""
+        s = Square(5)
+        s.update(id=42)
+        self.assertEqual(s.id, 42)
+
+        s.update(size=8)
+        self.assertEqual(s.width, 8)
+        self.assertEqual(s.height, 8)
+
+        s.update(x=3)
+        self.assertEqual(s.x, 3)
+
+        s.update(y=2)
+        self.assertEqual(s.y, 2)
+
+    def test_to_dictionary(self):
+        """Test the to_dictionary method of the Square class."""
+        s = Square(5, 2, 3, 10)
+        s_dict = s.to_dictionary()
+        expected_dict = {'id': 10, 'size': 5, 'x': 2, 'y': 3}
+        self.assertEqual(s_dict, expected_dict)
 
 
 if __name__ == "__main__":
