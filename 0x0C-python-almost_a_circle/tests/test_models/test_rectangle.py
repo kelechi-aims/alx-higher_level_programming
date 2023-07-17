1d39106719de8f68ab9f2d18e42fbdcb81702c18
kelechi-aims <kelechi@aims.edu.gh>
2023-07-17 18:44:38 +0100

    test for task-2

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+'''This module has the test cases for Rectangle class'''
+import unittest
+from models.rectangle import Rectangle
+
+
+class TestRectangle(unittest.TestCase):
+    '''Test suite for the Rectangle class.'''
+
+    def test_rectangle_creation(self):
+        '''
+        Test that a Rectangle instance is created with the correct id
+        '''
+        r1 = Rectangle(10, 2)
+        self.assertEqual(r1.id, 1)
+
+        r2 = Rectangle(2, 10)
+        self.assertEqual(r2.id, 2)
+
+        r3 = Rectangle(10, 2, 0, 0, 12)
+        self.assertEqual(r3.id, 12)
+
+    def test_getters_setters(self):
+        '''
+        Test the getters and setters for the width, height, x and y
+        attributes.
+        '''
+        r = Rectangle(10, 5)
+        self.assertEqual(r.width, 10)
+        self.assertEqual(r.height, 5)
+        self.assertEqual(r.x, 0)
+        self.assertEqual(r.y, 0)
+
+        r.width = 20
+        r.height = 8
+        r.x = 2
+        r.y = 3
+        self.assertEqual(r.width, 20)
+        self.assertEqual(r.height, 8)
+        self.assertEqual(r.x, 2)
+        self.assertEqual(r.y, 3)
+
+if __name__ == "__main__":
+    unittest.main()
