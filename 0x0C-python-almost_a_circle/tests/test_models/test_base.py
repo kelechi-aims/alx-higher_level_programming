be80a8b056b72f18e8f617aec4cfe40f3eab84cf
kelechi-aims <kelechi@aims.edu.gh>
2023-07-18 02:11:47 +0100

    task-18

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -166,5 +166,29 @@ class TestBase(unittest.TestCase):
         self.assertEqual(s1.x, 1)
         self.assertEqual(s1.y, 0)
 
+    def test_load_from_file_rectangle(self):
+        """Test load_from_file method with Rectangle instances."""
+        r1 = Rectangle(10, 7, 2, 8)
+        r2 = Rectangle(2, 4)
+        list_rectangles_input = [r1, r2]
+        Rectangle.save_to_file(list_rectangles_input)
+
+        list_rectangles_output = Rectangle.load_from_file()
+
+        for rect in list_rectangles_input:
+            self.assertIn(rect, list_rectangles_output)
+
+    def test_load_from_file_square(self):
+        """Test load_from_file method with Square instances."""
+        s1 = Square(5)
+        s2 = Square(7, 9, 1)
+        list_squares_input = [s1, s2]
+        Square.save_to_file(list_squares_input)
+
+        list_squares_output = Square.load_from_file()
+
+        for square in list_squares_input:
+            self.assertIn(square, list_squares_output)
+
 if __name__ == "__main__":
     unittest.main()
