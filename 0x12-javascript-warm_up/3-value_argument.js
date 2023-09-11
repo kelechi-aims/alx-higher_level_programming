642bdc5d80656067f1e8534acd2b9282c169f484
kelechi-aims <kelechi@aims.edu.gh>
2023-09-11 18:00:31 +0100

    task-3

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+/*
+ A script that prints the first argument passed to it
+*/
+if (process.argv[2]) {
+  console.log(process.argv[2]);
+} else {
+  console.log('No arguments');
+}
