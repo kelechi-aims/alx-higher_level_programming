fbab7d4f49595fa8f06a76f70bb7517458f07e4b
kelechi-aims <kelechi@aims.edu.gh>
2023-09-11 20:14:36 +0100

    task-8

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -3,7 +3,7 @@
  A script that prints x times “C is fun”
 */
 const num = parseInt(process.argv[2]);
-if (!isNaN(num) && num > 0) {
+if (!isNaN(num)) {
   for (let i = 0; i < num; i++) {
     console.log('C is fun');
   }
