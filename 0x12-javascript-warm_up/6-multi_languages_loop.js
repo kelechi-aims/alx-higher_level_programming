45a8a15ac534de7ecc1d4f8579149176f94b9b8b
kelechi-aims <kelechi@aims.edu.gh>
2023-09-11 19:35:41 +0100

    task-6

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+/*
+ A script that prints 3 lines: (like 1-multi_languages.js)
+ but by using an array of string and a loop
+*/
+const lines = [
+  'C is fun',
+  'Python is cool',
+  'JavaScript is amazing'
+];
+for (let i = 0; i < lines.length; i++) {
+  console.log(lines[i]);
+}
