ce43fd1a1072e706cc54ef2949c653cbe4bf78c3
kelechi-aims <kelechi@aims.edu.gh>
2023-09-17 05:37:44 +0100

    task-7 correction

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -10,7 +10,7 @@ from sqlalchemy.orm import sessionmaker
 
 
 if __name__ == '__main__':
-    db_url = f"mysql://{argv[1]}:{argv[2]}@localhost:3306/{argv[3]}"
+    db_url = f"mysql+mysqldb://{argv[1]}:{argv[2]}@localhost:3306/{argv[3]}"
     engine = create_engine(db_url)
     Session = sessionmaker(bind=engine)
     session = Session()
@@ -18,6 +18,6 @@ if __name__ == '__main__':
     states = session.query(State).order_by(State.id).all()
 
     for state in states:
-        print(state.id, state.name)
+        print("{}: {}".format(state.id, state.name))
 
     session.close()
