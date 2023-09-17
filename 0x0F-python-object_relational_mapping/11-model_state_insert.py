a6a636bc7c49ec0e41f1420e49ac83ec6754c396
kelechi-aims <kelechi@aims.edu.gh>
2023-09-17 06:20:28 +0100

    task-11

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+'''
+A script that prints the State objects with the name passed as argument
+from the database hbtn_0e_6_usa
+You must use the module SQLAlchemy
+'''
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == '__main__':
+    db_url = f"mysql+mysqldb://{argv[1]}:{argv[2]}@localhost:3306/{argv[3]}"
+    engine = create_engine(db_url)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    new_state = State(name='Louisiana')
+    session.add(new_state)
+    session.commit()
+    print(new_state.id)
+
+    session.close()
