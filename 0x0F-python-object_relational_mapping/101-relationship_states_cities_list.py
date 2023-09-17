d20c28a71dbd5a102f22e97c5f44afd20044dab3
kelechi-aims <kelechi@aims.edu.gh>
2023-09-17 09:15:12 +0100

    task-16

---
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+'''
+A script that lists all State objects, and corresponding City objects,
+contained in the database hbtn_0e_101_usa
+'''
+from sys import argv
+from relationship_state import Base, State
+from relationship_city import City
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == '__main__':
+    db_url = f"mysql+mysqldb://{argv[1]}:{argv[2]}@localhost:3306/{argv[3]}"
+    engine = create_engine(db_url)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    states = session.query(State).join(City).order_by(State.id, City.id).all()
+
+    for state in states:
+        print(f"{state.id}: {state.name}")
+        for city in state.cities:
+            print(f"    {city.id}: {city.name}")
+
+    session.close()
