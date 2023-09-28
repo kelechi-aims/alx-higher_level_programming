927ca19283973e22371c95ccb77740fb576042c9
kelechi-aims <kelechi.denise@yahoo.com>
2023-09-28 14:38:43 -0700

    task-0 fixed

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Sends a request to that URL, and displays the size of the body of the response
-curl -sI "$1" | grep "Content-Length:" | cut -d " " -2f
+curl -so /dev/null -w "%{size_download}\n" $1
