5816ce3d49f80d81411491d7c3ed959e49f5c500
kelechi-aims <kelechi.denise@yahoo.com>
2023-09-29 20:17:03 -0700

    task-1 fixed

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -8,7 +8,6 @@ import sys
 
 url = sys.argv[1]
 
-if __name__ == "__main__":
-    with request.urlopen(url) as response:
-        x_request_id = response.getheader('X-Request-Id')
-        print(x_request_id)
+with request.urlopen(url) as response:
+    x_request_id = response.getheader('X-Request-Id')
+    print(x_request_id)
