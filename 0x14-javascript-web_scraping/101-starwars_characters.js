87db88f867a250446ce3fbd4364018c2f7d92a9f
kelechi-aims <kelechi@aims.edu.gh>
2023-10-25 00:11:03 +0100

    task-8

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -7,17 +7,21 @@ request(apiUrl, (error, response, body) => {
   if (error) {
     console.error(error);
   } else {
-    const movieData = JSON.parse(body);
-
-    movieData.characters.forEach(characterUrl => {
-      request(characterUrl, (charError, charResponse, charBody) => {
-        if (charError) {
-          console.error(charError);
-        } else {
-          const characterData = JSON.parse(charBody);
-          console.log(characterData.name);
-        }
-      });
-    });
+    const characters = JSON.parse(body).characters;
+    printMovieData(characters, 0);
   }
 });
+
+function printMovieData (characters, index) {
+  request(characters[index], (charError, charResponse, charBody) => {
+    if (charError) {
+      console.error(charError);
+    } else {
+      const characterData = JSON.parse(charBody);
+      console.log(characterData.name);
+      if (index + 1 < characters.length) {
+        printCharacters(characters, index + 1);
+      }
+    }
+  });
+}
